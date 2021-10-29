feb43fe4a02e0cc3493f5b5a44528f626b562f63
Lance Liu <lanceliumeng89@gmail.com>
2021-10-29 18:32:20 +1100

    update html file for CORS Policy Error issue

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (window.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass('sticky');
+        }
+    })
+})
